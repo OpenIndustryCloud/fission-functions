87be61996103c0dcf59908d13d599065d23e36b2
Samuel Cozannet <samnco@gmail.com>
2017-10-30 19:43:05 +0000

    add secret read for python functions

--- echo_read_secret/echo_read_secret.py
+++ echo_read_secret/echo_read_secret.py
@@ -1,23 +1,18 @@
 import base64
 from flask import request, jsonify
-from kubernetes import config
-config.load_incluster_config()
-from kubernetes import client
+from kubernetes import config, client
 
 
 def main():
     input = request.json
 
-    # config.load_incluster_config()
-
-    v1 = client.CoreV1Api()
-
     # This requires a secret to exist in the environment
     #     kubectl create secret generic echo --from-literal=content=foobar
+    config.load_incluster_config()
+
+    v1 = client.CoreV1Api()
     for secrets in v1.list_secret_for_all_namespaces().items:
         if secrets.metadata.name == 'echo':
             content = base64.b64decode(secrets.data['content'])
-
-    output = {'input': input, 'secret': content}
-
-    return jsonify(output)
+            output = {'input': input, 'secret': content.decode('utf-8')}
+            return jsonify(output)
